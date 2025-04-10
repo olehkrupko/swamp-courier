472ec1726c4331420cf87403a8fed43d144bfe92
Oleh Krupko <oleh.krupko@gmail.com>
2025-04-11 00:50:21 +0300

    Handler(admin_default): added filters

--- handlers/admin_default_handler.py
+++ handlers/admin_default_handler.py
@@ -2,23 +2,18 @@ from hashlib import sha256
 from os import getenv
 
 from aiogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, Message
+from magic_filter import F
 
 from config.dispatcher import dp
 from services.redis_service import RedisService
 from services.swamp_api_service import SwampApiService
 
 
-@dp.message()
+@dp.message(F.chat.func(lambda chat: chat.id == getenv("TELEGRAM_ADMIN_CHATID")) and F.text.contains("http"))
 async def admin_default_handler(message: Message) -> None:
     """Handler that only works for a specific chat ID."""
-    telegram_admin_chatid = getenv("TELEGRAM_ADMIN_CHATID")
-
-    if not telegram_admin_chatid:
+    if not getenv("TELEGRAM_ADMIN_CHATID"):
         raise ValueError("TELEGRAM_ADMIN_CHATID environment variable is not set.")
-    if message.chat.id != int(telegram_admin_chatid):
-        raise PermissionError("Access denied.")
-    if "http" not in message.text:
-        raise ValueError("URL required.")
 
     href = message.text
 
@@ -27,7 +22,7 @@ async def admin_default_handler(message: Message) -> None:
 
     reply = ""
     if feed["similar_feeds"]:
-        reply += f"<b>[SIMILAR FEEDS PRESENTS, CANT SAVE]</b>\n\n"
+        reply += f"<b>[SIMILAR FEED(S) PRESENT, CANT SAVE]</b>\n\n"
 
     reply += "<b>EXPLAINED</b>:\n"
     reply += f"- {feed['explained']['title']}\n"
@@ -46,7 +41,7 @@ async def admin_default_handler(message: Message) -> None:
         reply += "\n"
 
     if feed["similar_feeds"]:
-        reply += f"<b>[SIMILAR FEEDS PRESENTS, CANT SAVE]</b>"
+        reply += f"<b>[SIMILAR FEED(S) PRESENT, CANT SAVE]</b>"
 
     # Generate a unique identifier for the response
     href_id = sha256(href.encode()).hexdigest()[:16]
