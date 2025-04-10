337ccd38ca3bc944cb5acf3d307e5599123bd983
Oleh Krupko <oleh.krupko@gmail.com>
2025-04-10 17:00:17 +0300

    Handlers: admin_default_handler added

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,2 +1,3 @@
 from .start_handler import *
 from .echo_handler import *
+from .admin_default_handler import admin_default_handler
---
+++ handlers/admin_default_handler.py
@@ -0,0 +1,14 @@
+from os import getenv
+
+from aiogram.types import Message
+
+from config.dispatcher import dp
+
+
+@dp.message()
+async def admin_default_handler(message: Message) -> None:
+    """Handler that only works for a specific chat ID."""
+    if message.chat.id == getenv("SPECIFIC_CHAT_ID"):
+        await message.reply("You have access to this handler!")
+    else:
+        await message.reply("Access denied.")
