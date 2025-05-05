ed9e989f3150a6b659ef404c971949b5b5738c8f
Oleh Krupko <oleh.krupko@gmail.com>
2025-05-05 15:23:30 +0300

    Handlers: added is_admin util function

--- handlers/admin_http_handler.py
+++ handlers/admin_http_handler.py
@@ -13,10 +13,7 @@ from services.redis_service import RedisService
 from services.swamp_api_service import SwampApiService
 
 
-@dp.message(
-    F.chat.func(lambda chat: chat.id == getenv("TELEGRAM_CHATID"))
-    and F.text.contains("http")
-)
+@dp.message(F.chat.func(is_admin) and F.text.contains("http"))
 async def admin_http_handler(message: Message) -> None:
     """Handler that only works for links sent to a specific chat ID."""
     if not getenv("TELEGRAM_CHATID"):
--- handlers/admin_save_callback.py
+++ handlers/admin_save_callback.py
@@ -1,5 +1,6 @@
 from aiogram.types import CallbackQuery
 from config.dispatcher import dp
+from magic_filter import F
 from services.redis_service import RedisService
 from services.swamp_api_service import SwampApiService
 
@@ -7,7 +8,7 @@ from services.swamp_api_service import SwampApiService
 # requires TELEGRAM_AUTOCONFIRM=false to function
 # if it's true, feed will be authomatically saved
 # and the button won't be shown
-@dp.callback_query(lambda c: c.data and c.data.startswith("admin-save:"))
+@dp.callback_query(F.chat.func(is_admin) and lambda c: c.data and c.data.startswith("admin-save:"))
 async def admin_save_callback(callback_query: CallbackQuery) -> None:
     """Process Save button callback for admin_http_handler."""
     href_id = callback_query.data.split("admin-save:")[1]
