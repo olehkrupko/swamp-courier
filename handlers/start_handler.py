9caee924f811a90cecc088472c58f2203613e885
Oleh Krupko <oleh.krupko@gmail.com>
2025-05-05 12:28:11 +0300

    Handlers(start): readability adjustments

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -10,10 +10,11 @@ async def command_start_handler(message: Message) -> None:
     """This handler receives messages with `/start` command."""
     reply = (
         f"Hello, {html.bold(message.from_user.full_name)}!\n"
+        "\n"
         "I am a bot that helps you to save feeds to swamp-api and optionally receive updates from there.\n"
         "Feel free to check out my code at https://github.com/olehkrupko/swamp-courier\n"
         "\n"
-        f"Your Chat ID: `{ html.bold(message.chat.id) }`"
+        f"Your Chat ID: { html.bold(message.chat.id) }"
     )
 
     await message.reply(reply)
