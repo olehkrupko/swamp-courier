8cc88586f48ec0cec74632eb5cf291316d1ce8e0
Oleh Krupko <oleh.krupko@gmail.com>
2025-11-16 22:46:14 +0200

    Services(Sentry): Enable Bugsink

--- run.py
+++ run.py
@@ -4,12 +4,32 @@ import sys
 from datetime import datetime
 from os import getenv
 
+import sentry_sdk
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
 import handlers
 from middlewares.error_handling_middleware import ErrorHandlingMiddleware
+
+
+sentry_sdk.init(
+    dsn=getenv("SENTRY_SDK_DSN"),
+    # Add data like request headers and IP for users, if applicable;
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    send_default_pii=True,
+    # # Set traces_sample_rate to 1.0 to capture 100%
+    # # of transactions for tracing.
+    # traces_sample_rate=1.0,
+    # # To collect profiles for all profile sessions,
+    # # set `profile_session_sample_rate` to 1.0.
+    # profile_session_sample_rate=1.0,
+    # # Profiles will be automatically collected while
+    # # there is an active span.
+    # profile_lifecycle="trace",
+    # # Enable logs to be sent to Sentry
+    # enable_logs=True,
+)
 
 
 async def main() -> None:
