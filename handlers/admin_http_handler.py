6b318c8fb456ce048fa019ca90a2f000398f914d
Oleh Krupko <oleh.krupko@gmail.com>
2025-11-18 13:03:19 +0200

    Handlers(admin): skip for new feeds

--- handlers/admin_http_handler.py
+++ handlers/admin_http_handler.py
@@ -44,11 +44,12 @@ async def admin_http_handler(message: Message) -> None:
     reply += f"similar_feeds: {len(feed['similar_feeds'])}\n"
     for each in feed["similar_feeds"]:
         reply += f"<b>{each['_id']}</b>: {each['title']}\n"
-        each['_created'] = (
-            datetime.strptime(each['_created'], '%Y-%m-%d %H:%M:%S.%f')
-            .strftime('%Y-%m-%d %H:%M')
-        )
-        reply += f"    - created: {each['_created']}\n"
+        if each['_created']:
+            each['_created'] = (
+                datetime.strptime(each['_created'], '%Y-%m-%d %H:%M:%S.%f')
+                .strftime('%Y-%m-%d %H:%M')
+            )
+            reply += f"    - created: {each['_created']}\n"
         if each["title"] != feed["explained"]["title"]:
             reply += f"    - title: {each['title']}\n"
         if each["frequency"] != feed["explained"]["frequency"]:
