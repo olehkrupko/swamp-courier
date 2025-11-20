1a3fe0ab2a86e9ee5ec71a4ef894bb4f193d50d4
Oleh Krupko <oleh.krupko@gmail.com>
2025-11-21 01:34:21 +0200

    Handlers(admin): support less precise datetime

--- handlers/admin_http_handler.py
+++ handlers/admin_http_handler.py
@@ -46,7 +46,7 @@ async def admin_http_handler(message: Message) -> None:
         reply += f"<b>{each['_id']}</b>: {each['title']}\n"
         if each["_created"]:
             each["_created"] = datetime.strptime(
-                each["_created"],
+                each["_created"] if "." in each["_created"] else each["_created"] + ".000000",
                 "%Y-%m-%d %H:%M:%S.%f",
             ).strftime("%Y-%m-%d %H:%M")
             reply += f"    - created: {each['_created']}\n"
